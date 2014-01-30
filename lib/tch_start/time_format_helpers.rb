89172cdefcabfbb0e1f5079c8d41eabe49ed5beb
Shaun Dern <shaun@substantial.com>
2014-01-30 09:30:46 -0800

    Fix time_format_helper_spec

--- lib/tch_start/time_format_helpers.rb
+++ lib/tch_start/time_format_helpers.rb
@@ -1,9 +1,17 @@
 module TimeFormatHelpers
   def format_time(time)
     total_hours = time / (60 * 60)
-    days = total_hours / 24
-    hours = total_hours % 24
+    return "less than an hour" if total_hours < 1
 
-    ""
+    days = (total_hours / 24).round
+    hours = (total_hours % 24).round
+
+    if days > 0
+      pretty_time = "#{days} days"
+      pretty_time << " and #{hours} hours" if hours > 0
+      pretty_time
+    else
+      "#{hours} hours"
+    end
   end
 end
--- spec/tch_start/time_format_helpers_spec.rb
+++ spec/tch_start/time_format_helpers_spec.rb
@@ -4,8 +4,20 @@ describe TimeFormatHelpers, "#format_time" do
   include TimeFormatHelpers
 
   let(:minute) { 60 }
+  let(:hour) { 60 * minute }
+  let(:day) { 24 * hour }
 
-  it "rounds up to hours" do
+  it "rounds down to hour" do
    format_time(1 * minute).should == 'less than an hour'
+  end
+
+  it "rounds down to the nearest hour" do
+    time = (1 * hour) + (45 * minute)
+    format_time(time).should == '1 hours'
+  end
+
+  it "includes days and hours" do
+    time = (2 * day) + (4 * hour)
+    format_time(time).should == '2 days and 4 hours'
   end
 end
