91ea3e59caf6036cb43309fb3ac79552a5b5d353
Aaron Jensen <aaronjensen@gmail.com>
2014-02-03 12:35:55 -0800

    move teaching-channel-start to teaching_channel_start

---
+++ lib/teaching_channel_start.rb
@@ -0,0 +1,27 @@
+require_relative 'teaching_channel_start/command'
+
+module TeachingChannelStart
+  VALID_ESTIMATES = [0, 1, 2, 4, 8, 16, 32, 64, 128]
+  WIP_LIMIT = 4
+  REPOS = %w[
+    TeachingChannel/teaching-channel
+    TeachingChannel/teaching-channel-blog
+    TeachingChannel/teaching-channel-chef
+  ]
+
+  class << self
+    attr_writer :cache_dir, :root_dir
+  end
+
+  def self.cache_dir
+    @cache_dir or raise "set cache_dir="
+  end
+
+  def self.root_dir
+    @root_dir or raise "set root_dir="
+  end
+
+  def self.cache
+    @cache ||= Cache.new(cache_dir)
+  end
+end
--- teaching-channel-start.gemspec
+++ teaching-channel-start.gemspec
@@ -1,7 +1,7 @@
 # coding: utf-8
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'teaching-channel-start/version'
+require 'teaching_channel_start/version'
 
 Gem::Specification.new do |spec|
   spec.name          = "teaching-channel-start"
