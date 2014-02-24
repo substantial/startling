726d8c7b2553686a970a4112d35b8c826401a7d9
Shaun Dern <shaun@substantial.com>
2014-02-24 10:54:55 -0500

    add CheckForLocalMods to commands

--- lib/teaching_channel_start/command.rb
+++ lib/teaching_channel_start/command.rb
@@ -6,12 +6,14 @@ require 'colored'
 
 require_relative 'cache'
 require_relative 'pivotal_tracker'
+
+require_relative "commands/base"
 require_relative 'commands/create_pull_request'
 require_relative 'commands/create_changelog'
 require_relative 'commands/print_usage'
 require_relative 'commands/check_wip'
 require_relative 'commands/start_story'
-require_relative "commands/base"
+require_relative 'commands/check_for_local_mods'
 
 # script/start
 #
@@ -21,23 +23,29 @@ require_relative "commands/base"
 
 module TeachingChannelStart
   class Command < Commands::Base
-    def initialize(args = ARGV)
-      super(args: args, pivotal_tracker: PivotalTracker.new(pivotal_api_token))
+
+    def self.run(attrs={})
+      attrs[:args] ||= ARGV
+      super(attrs)
     end
 
     def execute
-      check_for_local_mods
       Commands::PrintUsage.run(args: args)
+      Commands::CheckForLocalMods.run(git: git)
       set_pivotal_api_token
       Commands::CheckWip.run
       Commands::StartStory.run(story: story, pivotal_tracker: pivotal_tracker)
-      create_branch if branch_name != GitLocal.current_branch
+      create_branch if branch_name != git.current_branch
       Commands::CreateChangelog.run(story: story)
       Commands::CreatePullRequest.run(repo: repo, story: story, branch_name: branch_name)
     end
 
     def cache
       TeachingChannelStart.cache
+    end
+
+    def pivotal_tracker
+      @pivotal_tracker ||= PivotalTracker.new(pivotal_api_token)
     end
 
     def pivotal_api_token
@@ -49,25 +57,21 @@ module TeachingChannelStart
     end
     alias_method :set_pivotal_api_token, :pivotal_api_token
 
-    def check_for_local_mods
-      return if `git status --porcelain`.empty?
-
-      puts "Local modifications detected, please stash or something."
-      system("git status -s")
-      exit 1
-    end
-
     def create_branch
       puts "Creating branch #{branch_name}..."
-      GitLocal.create_remote_branch(branch_name, base_branch: "origin/#{default_branch}")
+      git.create_remote_branch(branch_name, base_branch: "origin/#{default_branch}")
     end
 
     def default_branch
       repo.default_branch
     end
 
+    def git
+      @git ||= GitLocal.new
+    end
+
     def repo
-      @repo ||= Github.repo(GitLocal.repo_name)
+      @repo ||= Github.repo(git.repo_name)
     end
 
     def story
@@ -91,8 +95,8 @@ module TeachingChannelStart
                end
 
       if branch.empty?
-        if GitLocal.current_branch_is_a_feature_branch?
-          return GitLocal.current_branch
+        if git.current_branch_is_a_feature_branch?
+          return git.current_branch
         else
           abort "Branch name must be specified when current branch is not feature/."
         end
---
+++ lib/teaching_channel_start/commands/check_for_local_mods.rb
@@ -0,0 +1,17 @@
+require_relative '../git_local'
+
+require_relative "base"
+
+module TeachingChannelStart
+  module Commands
+    class CheckForLocalMods < Base
+      def execute
+        return if git.status.empty?
+
+        puts "Local modifications detected, please stash or something."
+        system("git status -s")
+        exit 1
+      end
+    end
+  end
+end
