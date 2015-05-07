e8ed245f7a7d126af95e748eb9b4d88a7f3a73e7
Miles Starkenburg <miles@substantial.com>
2015-05-07 11:57:51 -0700

    Colorize once joined

--- lib/teaching_channel_start/commands/three_amigos.rb
+++ lib/teaching_channel_start/commands/three_amigos.rb
@@ -14,24 +14,24 @@ module TeachingChannelStart
           "2. When is the story complete?",
           "3. Can the story be broken down further?",
           "4. Do we need to track anything for the epic's goals?",
-        ].map { |string| string.white }.join("\n")
+        ].join("\n")
 
         question = [
           "Have these questions been answered (",
           'anything but "ole" will abort'.underline,
           ")? "
-        ].map { |string| string.yellow }.join
-
-        confirm = ask(question)
+        ].join
 
         puts
         puts "Time for 3 Amigos!!!"
         puts "If you need more info, checkout: #{wiki_link}"
         puts
-        puts "Three Amigo Guidelines".blue
+        puts "Three Amigo Guidelines".white
         puts
-        puts guidelines
+        puts guidelines.blue
         puts
+
+        confirm = ask(question.yellow)
         exit unless confirm == "ole"
       end
     end
