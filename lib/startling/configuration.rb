9bbf94502cb9ab69f76f95c5756bef50d183db12
Jeff Forde <jeff@substantial.com>
2015-09-23 14:02:52 -0700

    Fix glob

--- lib/startling/configuration.rb
+++ lib/startling/configuration.rb
@@ -37,7 +37,7 @@ module Startling
     end
 
     def self.load_hooks(path=DEFAULT_COMMAND_PATH)
-      command_dir = File.join(Startling::GitLocal.new.project_root, path "*")
+      command_dir = File.join(Startling::GitLocal.new.project_root, path, "*")
       return unless command_dir
       Dir.glob(command_dir).each do |command|
         load "#{command_dir}/#{command}"
