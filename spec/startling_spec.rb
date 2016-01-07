e4d93748da6aa45fd2fd3a2ecbd4516fa477a068
Miles Starkenburg <miles@substantial.com>
2016-01-07 13:56:12 -0800

    Fix specs

--- spec/startling_spec.rb
+++ spec/startling_spec.rb
@@ -2,19 +2,21 @@ require 'spec_helper'
 require 'fileutils'
 require 'startling'
 require 'startling/git_local'
+require 'startling/github'
 
 describe "bin/start" do
   let(:feature_name) { 'bin_start_starts_stories' }
   let(:feature_branch) { "feature/#{feature_name}" }
-  let(:repo_default_branch) { 'develop' }
+  let(:repo_default_branch) { 'master' }
   let(:git) { Startling::GitLocal.new }
-  let(:pivotal_story_id) { "#65074482" }
+  let(:repo) { Startling::Github.repo(git.repo_name) }
 
   before do
     test_repo_path = "tmp/test_repo"
     FileUtils.mkdir_p "tmp"
+
     unless File.exists? "tmp/test_repo/.git"
-      `git clone git@github.com:TeachingChannel/teaching-channel-start-testing.git #{test_repo_path}`
+      `git clone git@github.com:substantial/startling-testing.git #{test_repo_path}`
     end
 
     File.write File.join(test_repo_path, ".github_access_token"), Tokens.github
@@ -23,7 +25,7 @@ describe "bin/start" do
 
     Startling.root_dir = Startling.cache_dir = "."
 
-    git.checkout_branch 'develop'
+    git.checkout_branch repo_default_branch
     git.destroy_branch feature_branch
   end
 
@@ -33,24 +35,30 @@ describe "bin/start" do
 
   # VCR Preconditions:
   # * There should be no open pull requests:
-  #   https://github.com/TeachingChannel/startling-testing
-  # * The story should be estimated and unstarted:
-  #   https://www.pivotaltracker.com/story/show/65074482
-  #   Credentials for this project:
-  #     email: aaron+tchtesting@substantial.com
-  #     password: asdfasdf
-  #
+  #   https://github.com/substantial/startling-testing
 
-  it "starts stories from origin/develop",
+  it "starts stories from origin/master",
     vcr: { cassette_name: "bin_start_starts_stories" } do
-    allow_any_instance_of(Startling::Commands::ThreeAmigos)
-      .to receive(:ask) { "ole" }
 
-    command = Startling::Command.new(args: [pivotal_story_id, feature_name])
+    allow_any_instance_of(Startling::Handlers::DefaultPullRequestHandler)
+      .to receive(:ask)
+      .with("Please input a pull request title: ") { "The Title" }
+
+    allow_any_instance_of(Startling::Handlers::DefaultPullRequestHandler)
+      .to receive(:ask)
+      .with("Please input a pull request body: (default is empty)") { "The Body" }
+
+    allow_any_instance_of(Startling::Commands::CreateBranch)
+      .to receive(:ask)
+      .with("Enter branch name (enter for current branch): ") { feature_name }
+
+    command = Startling::Command.new(args: [])
     command.execute
+
+    git = Startling::GitLocal.new
 
     expect(git.remote_branches).to include feature_branch
     expect(git.current_branch).to eq feature_branch
-    expect(command.default_branch).to eq(repo_default_branch)
+    expect(repo.default_branch).to eq repo_default_branch
   end
 end
