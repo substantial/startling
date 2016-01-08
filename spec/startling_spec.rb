3230e0c16c9b8d49f519cd9f6bbec9b076d87835
Miles Starkenburg <miles@substantial.com>
2016-01-07 17:40:16 -0800

    Add spec for pull request body

--- spec/startling_spec.rb
+++ spec/startling_spec.rb
@@ -8,10 +8,56 @@ describe "bin/start" do
   let(:feature_name) { 'bin_start_starts_stories' }
   let(:feature_branch) { "feature/#{feature_name}" }
   let(:repo_default_branch) { 'master' }
+  let(:pull_request_body) { "This is a test body" }
   let(:git) { Startling::GitLocal.new }
   let(:repo) { Startling::Github.repo(git.repo_name) }
+  let(:pull_request) { repo.pull_request(feature_branch) }
 
   before do
+    local_configuration = <<CONFIG
+Startling.configure do |config|
+  # WIP Limit
+  # config.wip_limit = 4
+
+  # Repos to check against for WIP limit
+  # config.repos << 'substantial/startling-dev'
+
+  # Valid story estimations
+  # config.valid_estimates = [1, 2, 4, 8, 16, 32, 64, 128]
+
+  # Commands to be run before a story is stared
+  # config.hook_commands.before_story_start = [:check_wip]
+
+  # Command to be run after a story has started
+  # config.hook_commands.after_story_start = []
+
+  # Commands to be run before a pull request is created
+  # config.hook_commands.before_pull_request = []
+
+  # Commands to be run after a pull request is created
+  # config.hook_commands.after_pull_request = []
+
+  # Handler used to start a provider specific story related to the pull request
+  # config.story_handler = :pivotal_start
+
+  # Message for pull request commit
+  # config.pull_request_commit_message = "Startling"
+
+  # Message for pull request body
+  config.pull_request_body = "#{pull_request_body}"
+
+  # Labels for a pull request
+  # config.pull_request_labels = [WIP, REVIEW, HOLD]
+
+  # Handler used for setting the title and body of a pull request
+  config.pull_request_handler = :default_pull_request_handler
+end
+CONFIG
+
+    File.open('startlingfile.rb', 'w') { |file| file.write(local_configuration) }
+
+    Startling::Commands::Base.load_configuration
+
     test_repo_path = "tmp/test_repo"
     FileUtils.mkdir_p "tmp"
 
@@ -31,6 +77,7 @@ describe "bin/start" do
 
   after do
     FileUtils.cd "#{__dir__}/.."
+    FileUtils.rm "startlingfile.rb"
   end
 
   # VCR Preconditions:
@@ -44,9 +91,24 @@ describe "bin/start" do
       .to receive(:ask)
       .with("Please input a pull request title: ") { "The Title" }
 
+    allow_any_instance_of(Startling::Commands::CreateBranch)
+      .to receive(:ask)
+      .with("Enter branch name (enter for current branch): ") { feature_name }
+
+    command = Startling::Command.new(args: [])
+    command.execute
+
+    expect(git.remote_branches).to include feature_branch
+    expect(git.current_branch).to eq feature_branch
+    expect(repo.default_branch).to eq repo_default_branch
+  end
+
+  it "The pull request body is the same as the config",
+    vcr: { cassette_name: "bin_start_starts_stories_pr_body" } do
+
     allow_any_instance_of(Startling::Handlers::DefaultPullRequestHandler)
       .to receive(:ask)
-      .with("Please input a pull request body: (default is empty)") { "The Body" }
+      .with("Please input a pull request title: ") { "The Title" }
 
     allow_any_instance_of(Startling::Commands::CreateBranch)
       .to receive(:ask)
@@ -55,10 +117,6 @@ describe "bin/start" do
     command = Startling::Command.new(args: [])
     command.execute
 
-    git = Startling::GitLocal.new
-
-    expect(git.remote_branches).to include feature_branch
-    expect(git.current_branch).to eq feature_branch
-    expect(repo.default_branch).to eq repo_default_branch
+    expect(pull_request.attributes.body).to eq pull_request_body
   end
 end
