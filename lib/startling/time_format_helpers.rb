24d0fca3e1a780a9b9e13b6c341f2792607c80bd
Cassie Schmitz <schmitzc@gmail.com>
2016-02-19 13:01:36 -0800

    Re-add WIP check

---
+++ lib/startling/time_format_helpers.rb
@@ -0,0 +1,48 @@
+require 'tzinfo'
+require 'business_time'
+
+module Startling
+  module TimeFormatHelpers
+    def business_time_ago(time)
+      days = pdt(time).to_date.business_days_until(pdt(Time.now).to_date)
+
+      case days
+      when 0
+        hours_ago(time)
+      else
+        "#{pluralize(days, "day")} ago"
+      end
+    end
+
+    def hours_ago(time)
+      total_seconds = Time.now - time
+      time_suffix = total_seconds >= 0 ? 'ago' : 'from now'
+      total_seconds = total_seconds.abs
+
+      hours = (total_seconds / (60 * 60)).floor
+
+      if hours < 1
+        pretty_time = "less than an hour"
+      else
+        pretty_time = pluralize(hours, 'hour', 'hours')
+      end
+
+      "#{pretty_time} #{time_suffix}"
+    end
+
+    def pluralize(n, singular, plural=nil)
+      if n == 1
+        "1 #{singular}"
+      elsif plural
+        "#{n} #{plural}"
+      else
+        "#{n} #{singular}s"
+      end
+    end
+
+    def pdt(time)
+      tz = TZInfo::Timezone.get('America/Los_Angeles')
+      tz.utc_to_local(time)
+    end
+  end
+end
---
+++ lib/startling/work_printer.rb
@@ -0,0 +1,53 @@
+require 'paint'
+require_relative 'colorize_string'
+require_relative 'time_format_helpers'
+
+module Startling
+  class WorkPrinter
+    using ColorizeString
+    include TimeFormatHelpers
+
+    def print(works)
+      puts works.sort_by(&:started_at).map { |work| format_work(work) }.join("\n\n")
+      puts "\nThere is currently #{count(works)} out of #{Startling.wip_limit.to_s.yellow} WIP."
+    end
+
+    def format_pull_request(pull_request)
+      "#{format_pull_request_labels(pull_request)}: #{pull_request.title}\n" +
+        "  Started #{ago pull_request.created_at}, last updated #{ago pull_request.updated_at}\n" +
+        "  #{pull_request.url.cyan.underline}"
+    end
+
+    def format_work(work)
+      "#{work.authors.join(", ").green} - #{work.branch.to_s.yellow}\n".yellow +
+        indent(work.pull_requests.sort_by(&:created_at).map { |p| format_pull_request(p) }.join("\n"))
+    end
+
+    def indent(string)
+      string.split("\n").map {|s| "  #{s}"}.join("\n")
+    end
+
+    def ago(time)
+      business_time_ago(time).yellow
+    end
+
+    def count(works)
+      count = works.count
+      case count
+      when ->(c) { c > Startling.wip_limit }
+        count.to_s.red
+      when ->(c) { c == Startling.wip_limit }
+        count.to_s.yellow
+      else
+        count.to_s.blue
+      end
+    end
+
+    private
+    def format_pull_request_labels(pull_request)
+      pull_request.labels.map do |label|
+        Paint[label[:name], :black, label[:color]]
+      end.join(", ")
+    end
+  end
+end
