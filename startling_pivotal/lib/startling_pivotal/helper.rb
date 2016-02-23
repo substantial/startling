e6dd4092f2894b93be91ed47ef373da80e1e932b
Cassie Schmitz <schmitzc@gmail.com>
2016-02-22 18:22:46 -0800

    Remove PivotalTracker namespace

---
+++ startling_pivotal/lib/startling_pivotal/helper.rb
@@ -0,0 +1,13 @@
+require 'startling'
+
+module StartlingPivotal
+  class Helper
+    def api_token
+      @api_token ||= Startling.cache.fetch('.pivotal_api_token') do
+        username = ask("Enter your Pivotal Tracker username:  ")
+        password = ask("Enter your Pivotal Tracker password:  ") { |q| q.echo = false }
+        PivotalTracker::Api.api_token_for_user(username, password)
+      end
+    end
+  end
+end
