1d67c6d5d15b17440d2cf8a51f8459a9fbdf4d3c
Cassie Schmitz <schmitzc@gmail.com>
2016-02-23 14:13:31 -0800

    Update Startling config in Pivotal config generator

--- startling_pivotal/lib/generators/startling_pivotal/configuration_generator.rb
+++ startling_pivotal/lib/generators/startling_pivotal/configuration_generator.rb
@@ -1,5 +1,6 @@
 require 'rails/generators'
 require 'rails/generators/base'
+require 'startling'
 
 module StartlingPivotal
   module Generators
@@ -12,6 +13,21 @@ StartlingPivotal.configure do |config|
   # config.valid_estimates = [1, 2, 3]
 end
 CONFIG
+        end
+
+        Startling::Configuration::DEFAULT_STARTLINGFILES.each do |file_name|
+          if Dir.entries(Startling::GitLocal.new.project_root).include? file_name
+            inject_into_file file_name, before: 'Startling.configure do |config|' do
+<<CONFIG
+require 'startling_pivotal'
+
+CONFIG
+            end
+
+            gsub_file file_name,
+              '# config.story_handler = :pivotal_start',
+              'config.story_handler = :pivotal_start'
+          end
         end
       end
     end
