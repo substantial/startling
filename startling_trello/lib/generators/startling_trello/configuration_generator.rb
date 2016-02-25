190dfc7fb7a3e620ca7d1be5c6be61956692f309
Cassie Koomjian <schmitzc@gmail.com>
2016-02-25 11:23:09 -0800

    Put trello config values in startling config

--- startling_trello/lib/generators/startling_trello/configuration_generator.rb
+++ startling_trello/lib/generators/startling_trello/configuration_generator.rb
@@ -7,31 +7,30 @@ module StartlingTrello
   module Generators
     class ConfigurationGenerator < Rails::Generators::Base
       def create_config_file
-        create_file StartlingTrello::Configuration::DEFAULT_CONFIG_FILE do
+        generate 'startling:configuration'
+
+        file_name = 'startlingfile.rb'
+
+        inject_into_file file_name, before: 'Startling.configure do |config|' do
 <<CONFIG
-StartlingTrello.configure do |config|
+require 'startling_trello'
+
+CONFIG
+        end
+
+        gsub_file file_name,
+          '# config.story_handler = :pivotal_start',
+           'config.story_handler = :trello_start'
+
+        inject_into_file file_name, after: 'config.story_handler = :trello_start' do
+<<CONFIG
+
   # Trello Developer API key
   # config.developer_public_key = 'developer-public-key'
 
   # Trello Doing List ID
   # config.doing_list_id = 'doing-list-id'
-end
 CONFIG
-        end
-
-        Startling::Configuration::DEFAULT_STARTLINGFILES.each do |file_name|
-          if Dir.entries(Startling::GitLocal.new.project_root).include? file_name
-            inject_into_file file_name, before: 'Startling.configure do |config|' do
-<<CONFIG
-require 'startling_trello'
-
-CONFIG
-            end
-
-            gsub_file file_name,
-              '# config.story_handler = :pivotal_start',
-              'config.story_handler = :trello_start'
-          end
         end
       end
     end
